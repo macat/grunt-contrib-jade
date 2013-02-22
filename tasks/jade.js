eb3bf90fe3054c3e5e796e960c43a25fc32d5cdb
Attila Maczak <attila@maczak.hu>
2013-02-22 14:20:24 -0500

    Support client compile

--- tasks/jade.js
+++ tasks/jade.js
@@ -31,14 +31,15 @@ module.exports = function(grunt) {
     });
   });
 
-  var compileJade = function(srcFile, options, data) {
+  var jade = require('jade'),
+    compileJade = function(srcFile, options, data) {
     options = grunt.util._.extend({filename: srcFile}, options);
     delete options.data;
 
     var srcCode = grunt.file.read(srcFile);
 
     try {
-      return require('jade').compile(srcCode, options)(data);
+      return jade.compile(srcCode, options)(data);
     } catch (e) {
       grunt.log.error(e);
       grunt.fail.warn('Jade failed to compile.');
